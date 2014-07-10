00260c1c4f09355ef07231e24697cb16f95e5594
mattiaerre <mattia.richetto@gmail.com>
2014-07-10 16:32:13 +0100

    new playground project

--- EDE.Infrastructure/Listeners/IListener.cs
+++ EDE.Infrastructure/Listeners/IListener.cs
@@ -4,6 +4,6 @@ namespace EDE.Infrastructure.Listeners
 {
 	public interface IListener
 	{
-		void Handle(IDomainEvent domainEvent);
+		void Handle(IDomainEvent domainEvent); // rodo: rename to 'When'
 	}
 }
---
+++ EDE.Playground.Tests/DoubleDispatch_Tests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace EDE.Playground.Tests
+{
+	[TestFixture]
+	public class DoubleDispatch_Tests
+	{
+		private IListener _listener;
+
+		[SetUp]
+		public void Given_A_GlobalListener()
+		{
+			var handlers = new List<DomainEventHandler> { new LogResultChangedHandler(), new MailResultChangedHandler(), new LogGameStatusChangedHandler() };
+			_listener = new Listener(handlers);
+		}
+
+		[Test]
+		public void When_GameStatusChanged_Only_LogGameStatusChangedHandler_Should_Be_Called()
+		{
+			_listener.When(new GameStatusChanged());
+		}
+		
+		[Test]
+		public void When_ResultChanged_MailResultChangedHandler_And_LogResultChangedHandler_Should_Be_Called()
+		{
+			_listener.When(new ResultChanged());
+		}
+	}
+
+	public class GameStatusChanged : IDomainEvent
+	{
+	}
+
+	public class ResultChanged : IDomainEvent
+	{
+	}
+
+	public class Listener : IListener
+	{
+		private readonly IEnumerable<DomainEventHandler> _handlers;
+
+		public Listener(IEnumerable<DomainEventHandler> handlers)
+		{
+			_handlers = handlers;
+		}
+
+		public void When(IDomainEvent domainEvent)
+		{
+			foreach (var handler in _handlers)
+				handler.Apply(domainEvent);
+		}
+	}
+
+	public interface IListener
+	{
+		void When(IDomainEvent domainEvent);
+	}
+
+	public interface IDomainEvent
+	{
+	}
+
+	public abstract class DomainEventHandler
+	{
+		public void Apply(IDomainEvent domainEvent)
+		{
+			((dynamic)this).Handle((dynamic)domainEvent);
+		}
+
+		public void Handle(IDomainEvent domainEvent)
+		{
+		}
+	}
+
+	public class LogGameStatusChangedHandler : DomainEventHandler, IHandler<GameStatusChanged>
+	{
+		public void Handle(GameStatusChanged domainEvent)
+		{
+			Debug.WriteLine("log: game status changed");
+		}
+	}
+
+	public class MailResultChangedHandler : DomainEventHandler, IHandler<ResultChanged>
+	{
+		public void Handle(ResultChanged domainEvent)
+		{
+			Debug.WriteLine("mail: result changed");
+		}
+	}
+
+	public class LogResultChangedHandler : DomainEventHandler, IHandler<ResultChanged>
+	{
+		public void Handle(ResultChanged domainEvent)
+		{
+			Debug.WriteLine("log: result changed");
+		}
+	}
+
+	public interface IHandler<T> where T : IDomainEvent
+	{
+		void Handle(T domainEvent);
+	}
+}
